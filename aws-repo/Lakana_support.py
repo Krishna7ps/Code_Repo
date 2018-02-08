14eee08af8790b1646d6cfc32e08a18d51926274
Krishna Sabaris Pattamsetty <krishnapat@cybage.com>
2018-02-08 13:43:51 +0530

    changes from desktop

--- aws-repo/Lakana_support.py
+++ aws-repo/Lakana_support.py
@@ -28,9 +28,12 @@ import glob
 import fnmatch
 import selenium
 import django
+import xlwings
+import openpyxl
+import xlsxwriter
 
 
-#Setting up assumerole on boto3
+#1. Setting up assumerole on boto3
 
 sts=boto3.client('sts')
 res=sts.assume_role(RoleArn='arn:aws:iam::462397709356:role/lakana-fox-admin',RoleSessionName='testrole')
@@ -40,14 +43,14 @@ aws_secret_access_key=res['Credentials']['SecretAccessKey'],
 aws_session_token=res['Credentials']['SessionToken'])  
 
 
-#listing buckets and objects of each bucket
+#2. listing buckets and objects of each bucket
 
 for i in range(len(s3.list_buckets()['Buckets'])):
     buck=s3.list_buckets()['Buckets'][i]['Name']
     pprint.pprint(s3.list_objects(Bucket=buck))
     time.sleep(1000)
     
-#Listing each object that CacheControl metadata set
+#3. Listing each object that CacheControl metadata set
 
 API_list_buckets
 count=0
@@ -70,7 +73,7 @@ for i in range(len(s3.list_buckets()['Buckets'])):
         pass
 
 
-#listing buckets, objects, Keys 
+#4. listing buckets, objects, Keys 
 
 s3l=boto3.client('s3') #Creating s3 boto3 object
 s3_buckets=s3l.list_buckets() #Listing buckets under the account
@@ -94,7 +97,7 @@ s3.meta.client.download_file('mybucket', 'hello.txt', '/tmp/hello.txt')
 
 '''
 
-#Downloading objects from specific s3 bucket with specific word they start with
+#5. Downloading objects from specific s3 bucket with specific word they start with
 
 s3_objects=s3l.list_objects(Bucket='lakana-cloudformation')['Contents']
 no_of_objs=len(s3_objects)
@@ -105,14 +108,14 @@ for i in range(no_of_objs):
 
 
 
-#Deleting security groups from the account
+#6. Deleting security groups from the account
 
 
 ec2=boto3.client('ec2')
 
 sg=ec2.describe_security_groups()
 
-# print("List of security groups:")
+#print("List of security groups:")
 for i in range(len(sg['SecurityGroups'])):
     if sg['SecurityGroups'][i]['GroupName'] == 'default':
         print("Default SG can't be deleted")
@@ -125,7 +128,7 @@ for i in range(len(sg['SecurityGroups'])):
             print("Some error occurred while deleting ",sg['SecurityGroups'][i]['GroupName'])
 
 
-# Remove directory from git and local
+#7. Remove directory from git and local
 '''
 You could checkout 'master' with both directories;
 
@@ -140,3 +143,66 @@ In that case use:
 git rm -r --cached myFolder
 
 '''            
+
+#8. Solution for json serialize a datetime object and dumping dict object into an external json file.
+
+import datetime
+import json
+
+def myconverter(o):
+    if isinstance(o, datetime.datetime):
+        return o.__str__()
+
+with open('data.txt', 'w') as outfile:
+    json.dump(data, outfile, default=myconverter) #json.dumps(data,default=myconverter)
+
+#9. Listing instances Names, instance types
+
+import boto3
+import openpyxl as el
+ec2=boto3.client("ec2")
+
+data=ec2.describe_instances()   
+
+''' goto with #8 '''
+
+count=0
+wb=el.Workbook()
+sh1=wb.create_sheet("Avg CPU")
+sh1['A1']="Instace Name"
+sh1['B1']="Instance Type"
+sh1['C1']="State"
+sh1['D1']="Avg CPU"
+
+for i in range(len(data['Reservations'])):
+    for j in range(len(data['Reservations'][i]['Instances'])):
+        count=count+len(data['Reservations'][i]['Instances'])
+        for k in range(len(data['Reservations'][i]['Instances'][j]['Tags'])):
+            if data['Reservations'][i]['Instances'][j]['Tags'][k]['Key']=='Name':
+                ins_name=data['Reservations'][i]['Instances'][j]['Tags'][k]['Value'].strip()
+                ins_type=data['Reservations'][i]['Instances'][j]['InstanceType']
+                state=data['Reservations'][i]['Instances'][j]['State']['Name']
+                sh1.cell(row=count+1,column=1,value=ins_name)
+                sh1.cell(row=count+1,column=2,value=ins_type)
+                sh1.cell(row=count+1,column=3,value=state)
+wb.save("ins_info1.xlsx")
+wb.close()
+
+
+
+
+for i in range(len(data['Reservations'])):
+    for j in range(len(data['Reservations'][i]['Instances'])):
+        for k in range(len(data['Reservations'][i]['Instances'][j]['Tags'])):
+            if data['Reservations'][i]['Instances'][j]['Tags'][k]['Key']=='Name':
+                print(data['Reservations'][i]['Instances'][j]['Tags'][k]['Value'].strip(), (data['Reservations'][i]['Instances'][j]['InstanceType']))
+
+
+
+for i in range(len(data['Reservations'])):
+    for j in range(len(data['Reservations'][i]['Instances'])):
+        for k in range(len(data['Reservations'][i]['Instances'][j]['Tags'])):
+            if data['Reservations'][i]['Instances'][j]['Tags'][k]['Key']=='Name':
+                print(data['Reservations'][i]['Instances'][j]['Tags'][k]['Value'].strip(), (data['Reservations'][i]['Instances'][j]['InstanceType']))
+
+
