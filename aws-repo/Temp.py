eecd540a6898fc00c9c1c34fd205d84e2f2a10d3
krishna <krishna@email.com>
2018-10-12 22:23:16 +0530

    deleted temp.py files

--- aws-repo/Temp.py
+++
@@ -1,6 +0,0 @@
-s3l=boto3.client('s3') #Creating s3 boto3 object
-s3_objects=s3l.list_objects(Bucket='lakana-techops-assets-us-east-1')['Contents']
-no_of_objs=len(s3_objects)
-for i in range(no_of_objs):
-    obj_download=s3_objects[i]['Key']
-    s3l.download_file('lakana-techops-assets-us-east-1',obj_download,obj_download)
